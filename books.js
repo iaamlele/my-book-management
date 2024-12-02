9dd2a96a5c34a543e9ea1cb6a87eaf7109a4fbe3
Le Chen <LeChen11111@163.com>
2024-12-02 16:15:47 +0800

    first commit

---
+++ books.js
@@ -0,0 +1,19 @@
+export default book_information;
+
+const book_information = [
+    {
+        id: 1,
+        name: '人月神话'
+    },
+    {
+        id: 2,
+        name: '黑客与画家'
+    },
+    {
+        id: 3,
+        name: '像艺术家一样思考'
+    }, {
+        id: 4,
+        name: '编码'
+    }
+]
