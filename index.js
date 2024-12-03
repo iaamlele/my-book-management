6400d70bc74711a95223527cf049badf0091ba9a
Le Chen <LeChen11111@163.com>
2024-12-03 12:08:27 +0800

    Changing the code from synchronous to asynchronous

--- index.js
+++ index.js
@@ -13,26 +13,30 @@ app.get('/', (req, res) => {
 app.get("/getBookById", (req, res) => {
     const { id } = req.query;
     const book = search_books_by_id(parseInt(id));
-    console.log("查询到的书籍:", book);
-    if (book) {
-        res.json(book);
-    } else {
+
+    book.then((res1) => {
+        console.log('Query successful, book name:', res1);
+        res.json(res1);
+    }, (err1) => {
+        console.log('Query failed, err: ', err1);
         res.status(404).json({ message: "Book not found." });
-    }
+    })
 });
 
 // 更新接口
 app.post("/updateBookById", (req, res) => {
     const { id, newName } = req.body;
     console.log('id:', id, 'newName:', newName);
+
     const result = update_book_title_by_id(parseInt(id), newName);
-    if (result) {
-        console.log('Update successful:', result);
-        res.json(result);
-    } else {
-        console.log('Book not found for id:', id);
+    
+    result.then((res1) => {
+        console.log('Update successful:', res1);
+        res.json(res1);
+    }, (err1) => {
+        console.log('Book not found for id:', id, 'err:', err1);
         res.status(404).json({ message: "Book not found." });
-    }
+    })
 });
 
 // 启动服务
