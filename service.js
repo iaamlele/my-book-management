66ee47fcdd23af6f2758c42c4f043590e078412e
Le Chen <LeChen11111@163.com>
2024-12-03 11:51:57 +0800

    Change the code from synchronous to asynchronous, using Promise

--- service.js
+++ service.js
@@ -2,22 +2,42 @@
 import book_information from './books.js'
 
 function search_books_by_id(id) {
-    for(let i = 0; i < book_information.length; i++) {
-        if(book_information[i].id === id) {
-            return book_information[i].name;
-        }
-    }
-    return '查无此书';
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let book;
+            for(let i = 0; i < book_information.length; i++) {
+                if(book_information[i].id === id) {
+                    book = book_information[i].name;
+                }
+            }
+            if(book) {
+                resolve(book);
+            }else {
+                reject(new Error(''));
+            }
+        }, 1000)
+    })
+    
 }
 
 function update_book_title_by_id(id, name) {
-    for(let i = 0; i < book_information.length; i++) {
-        if(book_information[i].id === id) {
-            book_information[i].name = name;
-            return name;
-        }
-    }
-    return '查无此书';
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let flag = false;
+            for(let i = 0; i < book_information.length; i++) {
+                if(book_information[i].id === id) {
+                    false = true;
+                    book_information[i].name = name;
+                }
+            }
+            if(flag) {
+                resolve(book);
+            } else {
+                reject(new Error(''));
+            }
+        }, 1000)
+    }) 
 }
 
-export default {search_books_by_id, update_book_title_by_id};
+// 为什么这里不能加上default?
+export {search_books_by_id, update_book_title_by_id};
