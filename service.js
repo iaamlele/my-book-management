1f31fadffbe14d1b7deab7cf45007e5bfc7bfafa
Le Chen <LeChen11111@163.com>
2024-12-04 09:32:31 +0800

    Remove unnecessary asynchronous code

--- service.js
+++ service.js
@@ -2,41 +2,20 @@
 import book_information from './books.js'
 
 function search_books_by_id(id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let book;
-            for(let i = 0; i < book_information.length; i++) {
-                if(book_information[i].id === id) {
-                    book = book_information[i].name;
-                }
-            }
-            if(book) {
-                resolve(book);
-            }else {
-                reject(new Error(''));
-            }
-        }, 1000)
-    })
-    
+    for(let i = 0; i < book_information.length; i++) {
+        if(book_information[i].id === id) {
+            return book_information[i].name;
+        }
+    }
 }
 
 function update_book_title_by_id(id, name) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let flag = false;
-            for(let i = 0; i < book_information.length; i++) {
-                if(book_information[i].id === id) {
-                    false = true;
-                    book_information[i].name = name;
-                }
-            }
-            if(flag) {
-                resolve(book);
-            } else {
-                reject(new Error(''));
-            }
-        }, 1000)
-    }) 
+    for(let i = 0; i < book_information.length; i++) {
+        if(book_information[i].id === id) {
+            book_information[i].name = name;
+            return name;
+        }
+    }
 }
 
 // 为什么这里不能加上default?
