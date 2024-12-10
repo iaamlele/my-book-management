fa35555c178c0ecaba760c46390ee4b88718d15e
Le Chen <LeChen11111@163.com>
2024-12-10 18:03:26 +0800

    Implement display and update of books via GET.

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,14 +1,56 @@
-import book_information from "../data/books";
+const book_information = require("../data/books");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
-// 显示所有的图书
+
+exports.index = asyncHandler(async (req, res, next) => {
+    const numBooks = await Promise.all([
+        book_information.length
+    ]);
+    
+    res.render("index", {
+        title: "Local Library Home",
+        book_count: numBooks
+    })
+});
+
+// 查询所有的图书
 exports.book_list = asyncHandler(async (req, res, next) => {
     res.send("未实现：图书列表");
 });
 
 // 通过 GET 显示更新图书。
 exports.book_update_get = asyncHandler(async (req, res, next) => {
-    res.send("未实现：更新图书 GET");
+    async.parallel(
+        {
+            bookid: () => {
+                book_information.forEach(elem => {
+                    elem.id;
+                })
+            },
+            bookname: () => {
+                book_information.forEach(elem => {
+                    elem.name;
+                })
+            }
+        },
+        function(err, results) {
+            if(err) {
+                return next(err);
+            }
+            if(results.bookid === null) {
+                var err = new Error('Book not found');
+                err.status = 404;
+                return next(err);
+            }
+            res.render('book_form', {
+                title: 'Update Book',
+                book_id: results.id,
+                book_name: results.name
+            })
+        }
+    )
 });
   
 // 处理 POST 时的更新图书。
