1fadb15447501d648e3f9b42ba53f7298742425a
Le Chen <LeChen11111@163.com>
2024-12-11 10:39:33 +0800

    Add controller processing logic:get and post to request book information based on ID, get and post to request update of books

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,8 +1,5 @@
 const book_information = require("../data/books");
 const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator/check");
-const { sanitizeBody } = require("express-validator/filter");
-
 
 exports.index = asyncHandler(async (req, res, next) => {
     const numBooks = await Promise.all([
@@ -15,45 +12,59 @@ exports.index = asyncHandler(async (req, res, next) => {
     })
 });
 
-// 查询所有的图书
-exports.book_list = asyncHandler(async (req, res, next) => {
-    res.send("未实现：图书列表");
+// GET 根据ID查询图书
+exports.book_query_get = asyncHandler(async (req, res, next) => {
+    // 使用模板和数据创建并返回 HTML 文件
+    res.render('book_query');
+});
+
+// POST 根据ID查询图书
+exports.book_query_post = asyncHandler(async (req, res, next) => {
+    try{
+        const obj = req.body;
+        if(!obj) {
+            throw new Error('id为必填项!');
+        }
+        const id = obj.id;
+        
+        const bookname = book_information.find(elem => {
+            if(parseInt(elem.id) === parseInt(id)) {
+                return elem;
+            }
+        }).name;
+        if(!bookname) {
+            throw new Error('查询无此书!');
+        }
+        res.send(`查询成功, id为${id}的书名为: ${bookname}`);
+    } catch(err) {
+        res.status(400).send(`查找失败, 错误信息: ${err.message}`);
+    }
 });
 
 // 通过 GET 显示更新图书。
 exports.book_update_get = asyncHandler(async (req, res, next) => {
-    async.parallel(
-        {
-            bookid: () => {
-                book_information.forEach(elem => {
-                    elem.id;
-                })
-            },
-            bookname: () => {
-                book_information.forEach(elem => {
-                    elem.name;
-                })
-            }
-        },
-        function(err, results) {
-            if(err) {
-                return next(err);
-            }
-            if(results.bookid === null) {
-                var err = new Error('Book not found');
-                err.status = 404;
-                return next(err);
-            }
-            res.render('book_form', {
-                title: 'Update Book',
-                book_id: results.id,
-                book_name: results.name
-            })
-        }
-    )
+    res.render('book_update');
 });
   
 // 处理 POST 时的更新图书。
 exports.book_update_post = asyncHandler(async (req, res, next) => {
-    res.send("未实现：更新图书 POST");
+    try{
+        const obj = req.body;
+        if(!obj) {
+            throw new Error('ID和NewBookName为必填项!');
+        }
+        const id = obj.id;
+        const newbookname = obj.newbookname;
+        if(!id) {
+            throw new Error('查无此书!');
+        }
+        book_information.find(elem => {
+            if(parseInt(elem.id) === parseInt(id)) {
+                elem.name = newbookname;
+            }
+        })
+        res.send(`更新成功, 将id为${id}书名更新为: ${newbookname}`);
+    } catch(err) {
+        res.status(400).send(`更新失败, 错误信息: ${err.message}`);
+    }
 });
